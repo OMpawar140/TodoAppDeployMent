4a71685ea6bad1dc61d4bc45b488b756e421e8ac
siddhantmishra <siddhantmishra755@gmail.com>
2025-04-29 22:04:07 +0530

    ok

--- todo-app-backend/config/db.js
+++ todo-app-backend/config/db.js
@@ -2,7 +2,7 @@ const mysql = require('mysql2/promise');
 require('dotenv').config();
 
 const pool = mysql.createPool({
-  port: process.env.DB_PORT || 3307,
+  port: process.env.DB_PORT || 3306,
   host: process.env.DB_HOST || 'localhost',
   user: process.env.DB_USER || 'root',
   password: process.env.DB_PASSWORD || '',
